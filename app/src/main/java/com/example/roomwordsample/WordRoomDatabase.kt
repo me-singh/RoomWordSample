4657afa9f78e97afd56cae2e1b087a8974ce9f97
Amit Chauhan <amit@Amits-MacBook-Pro.local>
2020-02-20 00:43:15 +0530

    completed codelab - app is working with mvvm pattern (having room as backend for data)

--- app/src/main/java/com/example/roomwordsample/WordRoomDatabase.kt
+++ app/src/main/java/com/example/roomwordsample/WordRoomDatabase.kt
@@ -4,18 +4,46 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
 abstract class WordRoomDatabase : RoomDatabase() {
 
     abstract fun wordDao() : WordDao
 
+    private class WordDatabaseCallback(private val scope: CoroutineScope) : RoomDatabase.Callback() {
+
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(wordDao: WordDao) {
+            //Delete all content here
+            wordDao.truncate()
+
+            //Add Sample Word
+            var word = Word("Hello")
+            wordDao.insertWord(word)
+            word = Word("World")
+            wordDao.insertWord(word)
+
+            //add more words (if needed)
+        }
+    }
+
     companion object {
 
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase {
+        fun getDatabase(context: Context, scope: CoroutineScope): WordRoomDatabase {
             val tempInstance = INSTANCE
             if (tempInstance != null) {
                 return tempInstance
@@ -25,7 +53,8 @@ abstract class WordRoomDatabase : RoomDatabase() {
                     context.applicationContext,
                     WordRoomDatabase::class.java,
                     "Word_database"
-                ).build()
+                ).addCallback(WordDatabaseCallback(scope))
+                 .build()
                 INSTANCE = instance
                 return instance
             }
